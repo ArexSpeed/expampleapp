9ca693791a8554ac540824a0e38b2d0b3e4ba00a
Arek Cichocki <arekcichocki95@gmail.com>
2020-08-07 11:31:10 +0200

    Updates

---
+++ precache-manifest.adeb6401438821c3f6de4bc8d460eb06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d6aecba6281987f58c033b9bf38e60",
+    "url": "/expampleapp/index.html"
+  },
+  {
+    "revision": "10da1b6deea48cd126d3",
+    "url": "/expampleapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb43c5dd6ae9c5788a7b",
+    "url": "/expampleapp/static/js/2.da3882de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expampleapp/static/js/2.da3882de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10da1b6deea48cd126d3",
+    "url": "/expampleapp/static/js/main.6066bfd0.chunk.js"
+  },
+  {
+    "revision": "9543bba70985666bcfb6",
+    "url": "/expampleapp/static/js/runtime-main.73a7279f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/expampleapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
